ca6a2aa213a59a7537b37239f4f44ebd2559ae07
Johannes Weih <sheepr4ider@gmail.com>
2023-06-29 16:05:46 +0200

    feat: google oauth

--- app/src/app/auth/auth-config.module.ts
+++ app/src/app/auth/auth-config.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { AuthModule } from 'angular-auth-oidc-client';
 
-
 @NgModule({
     imports: [AuthModule.forRoot({
         config: {
               authority: 'https://accounts.google.com/o/oauth2/auth',
+              authWellknownEndpointUrl: 'https://accounts.google.com/.well-known/openid-configuration',
               redirectUrl: window.location.origin,
               postLogoutRedirectUri: window.location.origin,
-              clientId: 'please-enter-clientId',
-              scope: 'please-enter-scopes', // 'openid profile offline_access ' + your scopes
-              responseType: 'code',
+              clientId: '823450303414-o3mn8p9pth2o2ng5g7l5o40vhrjl14qv.apps.googleusercontent.com',
+              responseType: 'id_token token',
+              scope: 'openid email profile',
+              // responseType: 'code',
               silentRenew: true,
               useRefreshToken: true,
-              renewTimeBeforeTokenExpiresInSeconds: 30,
+              renewTimeBeforeTokenExpiresInSeconds: 30
           }
       })],
     exports: [AuthModule],
